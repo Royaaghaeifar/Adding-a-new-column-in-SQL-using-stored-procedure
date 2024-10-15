f5ffd0fc75b27ee614d99b2984fbdda733201563
Roya Aghaeifar <71782956+Royaaghaeifar@users.noreply.github.com>
2024-10-15 12:04:37 -0400

    Add files via upload

---
+++ Bin_Procedure.sql
@@ -0,0 +1,23 @@
+CREATE OR REPLACE PROCEDURE bin_hist(
+    table_name IN VARCHAR2,
+    column1    IN VARCHAR2, 
+    bin_value  IN NUMBER,   
+    max_value  IN NUMBER,  
+    min_value  IN NUMBER)
+IS
+    sql_statement VARCHAR2(4000);
+BEGIN
+    sql_statement := 'alter table ' || table_name || ' add (bin_col VARCHAR2(40))'; 
+      EXECUTE IMMEDIATE sql_statement;
+      
+    sql_statement := 'update ' || table_name || ' set bin_col =  case ' ||
+    'when floor(' || column1 || ' / :bin_value) * :bin_value > :max_value then ''>'' || :max_value ' ||
+    'when floor(' || column1 || ' / :bin_value) * :bin_value < :min_value then ''<'' || :min_value ' ||
+    'else to_char(floor(' || column1 || ' / :bin_value) * :bin_value) ' ||
+    'end'; 
+    
+    EXECUTE IMMEDIATE sql_statement 
+    USING bin_value, bin_value, bin_value, bin_value, bin_value, bin_value, max_value, max_value, min_value, min_value;
+    
+    COMMIT;
+END;
